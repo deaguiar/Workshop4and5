ef7f5df0e45280119e8ae55335a292262e55cde7
Henrique Aguiar <deaguiar.h@gmail.com>
2016-11-07 22:43:05 -0200

    fb6

--- app/components/commentthread.js
+++ app/components/commentthread.js
@@ -3,19 +3,19 @@ import CommentEntry from './commententry';
 
 export default class CommentThread extends React.Component {
   render() {
-    return (
-      <ul className="media-list">
-        {React.Children.map(this.props.children, function(child) {
-    return (
-      <li className="media">
-          {child}
-      </li>
-            )
-              })}
-              <li className="media">
-              <CommentEntry />
-              </li>
-              </ul>
-            )
-          }
-        }
+return (
+<ul className="media-list">
+{React.Children.map(this.props.children, function(child) {
+return (
+<li className="media">
+{child}
+</li>
+)
+})}
+<li className="media">
+<CommentEntry onPost={this.props.onPost} />
+</li>
+</ul>
+)
+}
+}
--- app/components/feeditem.js
+++ app/components/feeditem.js
@@ -2,11 +2,63 @@ import React from 'react';
 import StatusUpdate from './statusupdate';
 import CommentThread from './commentthread';
 import Comment from './comment';
+import {postComment,likeFeedItem, unlikeFeedItem} from '../server';
 
 export default class FeedItem extends React.Component {
+  constructor(props) {
+super(props);
+// The FeedItem's initial state is what the Feed passed to us.
+this.state = props.data;
+}
+handleCommentPost(commentText) {
+// Post a comment as user ID 4, which is our mock user!
+postComment(this.state._id, 4, commentText, (updatedFeedItem) => {
+// Update our state to trigger a re-render.
+this.setState(updatedFeedItem);
+});
+}
+handleLikeClick(clickEvent) {
+// Stop the event from propagating up the DOM
+// tree, since we handle it here. Also prevents
+// the link click from causing the page to scroll to the top.
+clickEvent.preventDefault();
+if (clickEvent.button === 0) {
+var callbackFunction = (updatedLikeCounter) => {
+this.setState({likeCounter: updatedLikeCounter});
+};
+if (this.didUserLike()) {
+// User clicked 'unlike' button.
+unlikeFeedItem(this.state._id, 4, callbackFunction);
+} else {
+// User clicked 'like' button.
+likeFeedItem(this.state._id, 4, callbackFunction);
+}
+}
+}
+/**
+* Returns 'true' if the user liked the item.
+* Returns 'false' if the user has not liked the item.
+*/
+didUserLike() {
+var likeCounter = this.state.likeCounter;
+var liked = false;
+// Look for a likeCounter entry with userId 4 -- which is the
+// current user.
+for (var i = 0; i < likeCounter.length; i++) {
+if (likeCounter[i]._id === 4) {
+liked = true;
+break;
+}
+}
+return liked;
+}
 render() {
-  var data = this.props.data;
-  var contents;
+var data = this.props.data;
+var contents;
+var likeButtonText = "Like";
+if (this.didUserLike()) {
+likeButtonText = "Unlike";
+  }
   switch(data.type) {
   case "statusUpdate":
   // Create a StatusUpdate. Dynamically created
@@ -14,14 +66,13 @@ render() {
   // Keys only need to be unique among *siblings*,
   // so we can re-use the same key as the FeedItem.
   contents = (
-  <StatusUpdate key={data._id}
-  author={data.contents.author}
-  postDate={data.contents.postDate}
-  location={data.contents.location}>
-  {data.contents.contents}
-  </StatusUpdate>
-  );
-  break;
+                   <StatusUpdate key={data._id} author={data.contents.author} postDate={data.contents.postDate} location={data.contents.location}>
+                   {data.contents.contents.split("\n").map((line, i) => {
+                     return ( <p key={"line" + i}>{line}</p>);
+                   })}
+                   </StatusUpdate>
+               );
+               break;
   default:
   throw new Error("Unknown FeedItem: " + data.type);
   }
@@ -34,7 +85,7 @@ render() {
   <div className="col-md-12">
   <ul className="list-inline">
   <li>
-  <a href="#">
+  <a href="#" onClick={(e) => this.handleLikeClick(e)}>
   <span className="glyphicon glyphicon-thumbs-up">
   </span> Like</a>
   </li>
@@ -57,25 +108,23 @@ render() {
   <div className="panel-footer">
   <div className="row">
   <div className="col-md-12">
-  <a href="#">{data.likeCounter.length} people</a>
+  <a href="#"> {data.likeCounter.length} people </a>
   like this
   </div>
   </div>
-  <hr />
-  <CommentThread>
-  { data.comments.map((comment, i) => {
-// i is comment's index in comments array
-return (
-<Comment key={i}
-author={comment.author}
-postDate={comment.postDate}>
-{comment.contents}
-</Comment>
-);
+  <hr/>
+    <CommentThread onPost={(commentText) =>
+      this.handleCommentPost(commentText)}>
+      {data.comments.map((comment, i) => {
+   return (<Comment key={i}
+     author={comment.author}
+     postDate={comment.postDate}>{comment.contents}
+   </Comment>
+   );
 })}
   </CommentThread>
+     </div>
   </div>
-  </div>
-  )
-
+    )
+  }
 }
